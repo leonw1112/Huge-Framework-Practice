29fca44f25840a85e4ee645c90caa614c5d2eb45
leonw1112 <leon.tobias.steiermark@gmail.com>
2026-06-01 09:34:11 +0200

    Refactor NoteModel to use mysqli for database connections and add DatabaseFactoryMySQLi class

--- application/model/NoteModel.php
+++ application/model/NoteModel.php
@@ -22,21 +22,22 @@ class NoteModel
         return $query->fetchAll();
     }
 
-    /**
+/**
      * Get a single note
      * @param int $note_id id of the specific note
      * @return object a single object (the result)
      */
     public static function getNote($note_id)
     {
-        $database = DatabaseFactory::getFactory()->getConnection();
+        $database = DatabaseFactoryMySQLi::getFactory()->getConnection();
 
-        $sql = "SELECT user_id, note_id, note_text FROM notes WHERE user_id = :user_id AND note_id = :note_id LIMIT 1";
-        $query = $database->prepare($sql);
-        $query->execute(array(':user_id' => Session::get('user_id'), ':note_id' => $note_id));
+        $sql = "SELECT user_id, note_id, note_text FROM notes WHERE user_id = ? AND note_id = ? LIMIT 1";
+        $stmt = $database->prepare($sql);
+        $stmt->bind_param('ii', Session::get('user_id'), $note_id);
+        $stmt->execute();
 
-        // fetch() is the PDO method that gets a single result
-        return $query->fetch();
+        // get_result()->fetch_object() is the mysqli method that gets a single result
+        return $stmt->get_result()->fetch_object();
     }
 
     /**
