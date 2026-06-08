f826428915d5455b61043aec115b0d2557dc444d
leonw1112 <leon.tobias.steiermark@gmail.com>
2026-06-08 14:21:50 +0200

    Display gallery images in the gallery view and handle empty state message

--- application/view/gallery/index.php
+++ application/view/gallery/index.php
@@ -13,7 +13,16 @@
         <!-- TODO: Display gallery images here -->
         <div class="gallery-grid">
             <!-- Gallery images will be displayed here -->
-             
+            <?php if (!empty($this->photos)) { ?>
+                <?php foreach ($this->photos as $photo) { ?>
+                    <div class="gallery-item">
+                        <img src="<?php echo Config::get('URL'); ?>uploads/<?php echo htmlspecialchars($photo->filename); ?>" alt="<?php echo htmlspecialchars($photo->title); ?>">
+                        <p><?php echo htmlspecialchars($photo->title); ?></p>
+                    </div>
+                <?php } ?>
+            <?php } else { ?>
+                <p>Keine Bilder in der Galerie vorhanden.</p>
+            <?php } ?>
         </div>
 
     </div>
